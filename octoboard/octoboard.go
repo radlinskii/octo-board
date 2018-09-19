7c778dce800698248a96138b56b51b98abf9568e
radlinskii <radlinskiignacy@gmail.com>
2018-09-20 00:41:00 +0200

    Add url query parameters support

--- octoboard/octoboard.go
+++ octoboard/octoboard.go
@@ -9,8 +9,18 @@ import (
 
 var templates = template.Must(template.ParseFiles(filepath.Join("octoboard", "tmpl", "index.html")))
 
-func rootHandler(w http.ResponseWriter, _ *http.Request) {
-	err := templates.ExecuteTemplate(w, "index.html", nil)
+type Person struct {
+	Name string
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	names, ok := r.URL.Query()["name"]
+	if !ok || len(names[0]) < 1 {
+		log.Println("Url Parameter 'name' is missing")
+		return
+	}
+	name := names[0]
+	err := templates.ExecuteTemplate(w, "index.html", Person{name})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
